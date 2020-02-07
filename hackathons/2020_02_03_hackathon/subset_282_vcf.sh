a0301aaf8fd8965693a4c818626a488ae043be94
Merritt <merrittbburch@gmail.com>
2020-02-07 10:46:45 -0500

    hack

--- hackathons/2020_02_03_hackathon/subset_282_vcf.sh
+++ hackathons/2020_02_03_hackathon/subset_282_vcf.sh
@@ -14,7 +14,6 @@
 
 
 # Subset 282 SNPs
-# Subset nam founders
 for CHROM in {1..10}
 do
   echo "Start data subsetting on chromosome ${CHROM}"
@@ -22,3 +21,54 @@ do
   bcftools view -S /workdir/mbb262/goodman282/282set_lines.txt hmp321_282_agpv4_merged_chr${CHROM}.imputed.vcf.gz -Oz -o /workdir/mbb262/goodman282/hmp321_282_agpv4_merged_chr${CHROM}_imputed_goodman282.vcf.gz
   echo "End data subsetting on chromosome ${CHROM}"
 done
+
+# Filter SNPs based on imputation accuracy
+for CHROM in {1..10}
+do
+  echo "Start data filtering on chromosome ${CHROM}"
+  bcftools filter -e 'DR2<=0.8' hmp321_282_agpv4_merged_chr${CHROM}_imputed_goodman282.vcf.gz -o hmp321_282_agpv4_merged_chr${CHROM}_imputed_filteredDR2_goodman282.vcf.gz
+  echo "End data filtering on chromosome ${CHROM}"
+done
+
+# Run SNP GWAS within tassel 4
+for CHROM in {1..10}
+do
+  /programs/tassel4-standalone/run_pipeline.pl \
+    -Xmx220g \
+    -fork1 -importGuess /workdir/mbb262/goodman282/hmp321_282_agpv4_merged_chr${CHROM}_imputed_filteredDR2_goodman282.vcf.gz \
+    -fork2 -r /workdir/mbb262/goodman282/vcf_intersect_mdp_traits.txt \
+    -fork3 -ck -fork1 \
+    -combine4 -input1 -input2 -intersect \
+    -combine5 -input5 -input3 \
+    -mlm -mlmVarCompEst P3D -mlmCompressionLevel None \
+    -export /workdir/mbb262/goodman282/gwas_results/MLM_goodman282_kinship \
+    -runfork1 -runfork2 -runfork3
+    echo "End mlm on chromosome ${CHROM}"
+done
+
+
+# Generate kinship matrix for NAM
+/programs/tassel-5-standalone/run_pipeline.pl \
+  -debug /workdir/mbb262/goodman282/debug_kinship_chr10.log \
+  -Xmx220g \
+  -importGuess /workdir/mbb262/goodman282/hmp321_282_agpv4_merged_chr10_imputed_filteredDR2_goodman282.vcf \
+  -KinshipPlugin \
+  -method Centered_IBS \
+  -endPlugin \
+  -export /workdir/mbb262/goodman282/goodman282_kinship_chr10.txt \
+  -exportType SqrMatrix
+
+# Run mlm in tassel4
+/home/mbb262/bioinformatics/tassel4-standalone/run_pipeline.pl \
+  -Xmx500g \
+  -fork1 -importGuess /workdir/mbb262/goodman282/hmp321_282_agpv4_merged_chr10_imputed_filteredDR2_goodman282.vcf \
+  -fork2 -importGuess /workdir/mbb262/goodman282/vcf_intersect_mdp_traits.txt \
+  -fork3 -importGuess /workdir/mbb262/goodman282/goodman282_kinship_chr10.txt \
+  -combine4 -input1 -input2 -intersect \
+  -combine5 -input4 -input3 \
+  -mlm -mlmVarCompEst P3D -mlmCompressionLevel None \
+  -mlmOutputFile /workdir/mbb262/goodman282/gwas_results/MLM_goodman282_kinship \
+  -runfork1 -runfork2 -runfork3
+
+
+
