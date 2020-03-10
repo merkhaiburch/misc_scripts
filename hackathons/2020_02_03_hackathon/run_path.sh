c665ba22c9789d6c482144f2cc0679610547c18a
Merritt Burch <merrittbburch@gmail.com>
2020-03-10 15:35:21 -0400

    edits to run paths code for old PHG build

--- hackathons/2020_02_03_hackathon/run_path.sh
+++ hackathons/2020_02_03_hackathon/run_path.sh
@@ -1,8 +1,21 @@
-#1 config
-#2 consensus method
-#3 fastq Dir
+#!/bin/bash
 
-# NAM haplotypes after running consensus from DB, methods file maxDiv = 0.001 
+# ---------------------------------------------------------------
+# Author.... Merritt Burch
+# Contact... mbb262@cornell.edu
+# Date...... 2020-02-18 
+#
+# Description 
+#   - Script by Evan Long to 
+#   - 
+# ---------------------------------------------------------------
+
+# Notes
+# $1 config
+# $2 consensus method
+# $3 fastq Dir
+
+# Provides NAM haplotypes after running consensus from DB, methods file maxDiv = 0.001 
 /workdir/eml255/tools/tassel-5-standalone/run_pipeline.pl \
   -Xmx100g \
   -debug \
@@ -15,16 +28,19 @@
   -outputFile phg_pangenome \
   -endPlugin > PHG_pangeome.log
 
-# Index haplotypes fastqs using minimap
+
+# Indexes haplotype fastqs using minimap
 /programs/minimap2-2.17/minimap2 \
   -d pangenome_index  \
   -k 21 \
   -w 11 \
   -I 90G phg_pangenome.fa
 
+
 # Mapping 282 fastqs onto pangenome haplotypes (mapping 282 reads to collapsed PHG haplotypes)
 /workdir/eml255/tools/tassel-5-standalone/run_pipeline.pl \
-  -debug -Xmx200G \
+  -debug \
+  -Xmx200g \
   -configParameters $1 \
   -HaplotypeGraphBuilderPlugin \
   -configFile $1 \
@@ -41,6 +57,7 @@
   -methodDescription "dummy2" \
   -debugDir debug/ \
   -endplugin > PHG_dipPath.log
+
 
 #Find Paths
 /workdir/eml255/tools/tassel-5-standalone/run_pipeline.pl \
@@ -60,9 +77,12 @@
   -pathMethod "dummy2" \
   -endPlugin > PHG_Path.log
 
+
 # export paths
-/workdir/tassel-5-standalone/run_pipeline.pl  -debug \
-  -Xmx100g -configParameters config.txt \
+/workdir/tassel-5-standalone/run_pipeline.pl  \
+  -debug \
+  -Xmx100g \
+  -configParameters config.txt \
   -HaplotypeGraphBuilderPlugin \
   -configFile config.txt \
   -methods mummer4,refRegionGroup \
@@ -72,7 +92,8 @@
   -keyFile keyfile_pathing.txt \
   -outDir out_path \
   -readMethod "dummy2" \
-  -pathMethod "dummy2" -endPlugin
+  -pathMethod "dummy2" \
+  -endPlugin
 
 
 
