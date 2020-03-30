41c2ad67ac9f1968b73b1d0f79e45eac20f57c37
Merritt Burch <merrittbburch@gmail.com>
2020-03-30 13:34:10 -0400

    example lapply loop in R

---
+++ r_lapply_loop.R
@@ -0,0 +1,11 @@
+# Modify function to calculate ames only PCs within windows and return variances
+# Example on how to use lappy instead of for loops
+# temp <- function(x,y,z){
+#   lapply(seq_len(5), function(i) {
+#     #here x
+#     lapply(seq_len(3), function(j) {
+#       # here z
+#       message("Hi Merritt. I am on chromosome ", i, " and I am doing region: ", j)
+#     })
+#   })
+# }
