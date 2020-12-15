86f8e3d87463200b4081b920f44c86f47685e37a
Merritt Burch <merrittbburch@gmail.com>
2020-12-15 17:00:35 -0500

    sine and minimap

---
+++ hackathons/2020_12_tes/minimap_sines.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------
+# Author.... Merritt Burch
+# Contact... mbb262@cornell.edu
+# Date...... 2020-12-15 
+#
+# Description 
+#   - December 2020 hackathon: TE annotation group
+#   - Minimap only known SINE TEs to maize v5, compare with sine-finder results
+# ---------------------------------------------------------------
+
+# Get B73 v5 
+# wget https://download.maizegdb.org/Zm-B73-REFERENCE-NAM-5.0/Zm-B73-REFERENCE-NAM-5.0.fa.gz
+# gunzip *.gz
+
+
+# ----------------
+# Minimap stuff
+# ----------------
+
+# Get help
+# /programs/minimap2-2.17/minimap2 --help
+
+# Set path to program
+export PATH=/programs/minimap2-2.17:$PATH
+
+# Index the genome to save time
+minimap2 -d \
+    ./b73_v5.mmi \
+    ./Zm-B73-REFERENCE-NAM-5.0.fa 
+
+# Using general usage parameters
+minimap2 -ax sr \
+    -t 30 \
+    -A 1 \
+    -B 5 \
+    -O 39,81 \
+    -N 6000 \
+    -f  \
+    -k  \
+    --secondary=yes \
+    ./b73_v5.mmi \
+    ./maize_TE_db_exemplars.sine.fa > sine_alignment.sam
+
+
+# Count the number of matches
+wc -l sine_alignment.sam
+#wc -l temp.txt
+
+# View file
+vim sine_alignment.sam
+
+
--- hackathons/2020_12_tes/sine_burch.sh
+++ hackathons/2020_12_tes/sine_burch.sh
@@ -15,11 +15,92 @@ chmod 777
 ./annotate_sine.sh EP1 Zm00010a /workdir/mbb262/sine/Zm-EP1-REFERENCE-TUM-1.0.fa /workdir/mbb262/sine/maize.SINE.post-B73Ab10
 
 # Run second version of script
-./annotate_sine_2.sh \
-    EP1 Zm00010a \
-    /workdir/mbb262/sine/Zm-EP1-REFERENCE-TUM-1.0.fa \
-    /workdir/mbb262/sine/maize.SINE.post-B73Ab10 \
-    /workdir/mbb262/temp
+./annotate_sine.sh \
+    /workdir/mbb262/hack/EP1 \
+    Zm00010a \
+    /workdir/mbb262/hack/Zm-EP1-REFERENCE-TUM-1.0.fa \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-B73Ab10 \
+    /workdir/mbb262/hack
+
+
+# New code
+
+# Variables
+GENOME=EP1
+SHORTID=Zm00010a
+GENOMEFASTA=/workdir/mbb262/hack/Zm-EP1-REFERENCE-TUM-1.0.fa
+OUTDIR="/workdir/mbb262/hack/EP1/sine" ## need to remake because different from CONFIG
+OLDDB=/workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-B73Ab10
+TEMPDIR=/workdir/mbb262/hack/temp
+
+mkdir -p $OUTDIR
+mkdir -p $TEMPDIR
+
+# Load mmseq
+`export PATH=/programs/mmseqs-avx2-11/bin:$PATH`
+export PATH=/programs/mmseqs-avx2-11/bin:$PATH
+
+## now create a db for the new genome (in a directory because there's lots of output files)
+mkdir -p mmseqsDBs
+mmseqs createdb \
+    $OUTDIR/${GENOME}-matches.noTSD.fa \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/${GENOME}.SINE 
+
+## concatenate the old (from cmd line) to the new genome entries
+mmseqs concatdbs \
+    $OLDDB \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/${GENOME}.SINE \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.${GENOME}.combined
+
+mmseqs concatdbs \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-B73Ab10_h \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/${GENOME}.SINE_h \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.${GENOME}.combined_h
+
+
+#### then need to cluster using the updated clusters
+## kinda confusing list of arguments, shown here for clarity
+# mmseqs clusterupdate <oldDB> <newDB> <oldDB_clustering> <outDB> <tmpDir> [opts] ## was getting confused with oldDB_clustering and rewriting names!
+mmseqs clusterupdate \
+    $OLDDB \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.${GENOME}.combined \
+    $OLDDB.8080 \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-${GENOME} \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-${GENOME}.8080 \
+    $TEMPDIR \
+    --min-seq-id 0.8 -c 0.8 --cov-mode 1 --cluster-mode 2 --search-type 3
+
+mmseqs createtsv \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-${GENOME} \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-${GENOME} \
+    /workdir/mbb262/hack/annotate_tes/sine/mmseqsDBs/maize.SINE.post-${GENOME}.8080 \
+    /workdir/mbb262/hack/annotate_tes/sine/maize.SINE.post-${GENOME}.8080.tsv
+ 
+#mmseqs clusterupdate maize.SINE ${GENOME}.SINE maize.SINE.8080 maize.SINE.post-${GENOME} maize.SINE.post-${GENOME}.8080 /scratch/ --min-seq-id 0.8 -c 0.8 --cov-mode 1 --cluster-mode 2
+
+#mmseqs createtsv maize.SINE maize.SINE maize.SINE.8080 maize.SINE.8080.tsv
+ 
+#mmseqs createdb DB.fasta DB_new
+#mmseqs clusterupdate DB_trimmed DB_new DB_trimmed_clu DB_new_updated DB_update_clu tmp
+
+## then generate a gff3
+Rscript generate_gff_SINE.R $GENOME $SHORTID $OUTDIR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# OLD STUFF
 
 
 # Run code while supplying tempdir
