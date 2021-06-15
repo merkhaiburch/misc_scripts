26a0040102b7585b1ccfe6d64913d998ff0b9553
Merritt Burch <merrittbburch@gmail.com>
2021-06-15 10:00:16 -0400

    using bbduk from bbmap and fastqc

--- hackathons/2021_06_pandand_assemblies/trim_seq.sh
+++ hackathons/2021_06_pandand_assemblies/trim_seq.sh
@@ -27,83 +27,75 @@
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
     in1=zea_diploperennis_1.fq.gz \
     in2=zea_diploperennis_2.fq.gz \
-    out1=_clean1.fq \
-    out2=_clean2.fq
-
-mv MCRTL020_CKDL200164854-1a-AK6510-AK6670_HMGJWDSXY_L4_1.fq.gz zea_diploperennis_1.fq.gz
-mv MCRTL020_CKDL200164854-1a-AK6510-AK6670_HMGJWDSXY_L4_2.fq.gz zea_diploperennis_2.fq.gz
+    out1=zea_diploperennis_1_clean1.fq \
+    out2=zea_diploperennis_2_clean2.fq
 
 # Tripsacum dactyloides var floridanum (Buckler lab clone T007-0002) [?]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-mv T0007_USPD16080542-1_H3325CCXY_L3_1.fq.gz tripsacum_dactyloides_floridanum_1.fq.gz
-mv T0007_USPD16080542-1_H3325CCXY_L3_2.fq.gz tripsacum_dactyloides_floridanum_2.fq.gz
+    in1=tripsacum_dactyloides_floridanum_1.fq.gz \
+    in2=tripsacum_dactyloides_floridanum_2.fq.gz \
+    out1=tripsacum_dactyloides_floridanum_1_clean1.fq \
+    out2=tripsacum_dactyloides_floridanum_2_clean2.fq
 
 # Vossia cuspidata (Buckler lab clone A1077-0002) [?]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-mv A1077002_USPD16097230-AK4973-AK12977_HJM33DSXX_L4_1.fq.gz vossia_cuspidata_1.fq.gz
-mv A1077002_USPD16097230-AK4973-AK12977_HJM33DSXX_L4_2.fq.gz vossia_cuspidata_2.fq.gz
+    in1=vossia_cuspidata_1.fq.gz \
+    in2=vossia_cuspidata_2.fq.gz \
+    out1=vossia_cuspidata_1_clean1.fq \
+    out2=vossia_cuspidata_2_clean2.fq
 
 # Chrysopogon aciculatus (AUB 135) [AN21TSTL0006]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-mv MCRTL042_CKDL200164855-1B-AK6514-AK6680_HMJV7DSXY_L3_1.fq.gz chrysopogon_aciculatus_1.fq.gz
-mv MCRTL042_CKDL200164855-1B-AK6514-AK6680_HMJV7DSXY_L3_2.fq.gz chrysopogon_aciculatus_2.fq.gz
+    in1=chrysopogon_aciculatus_1.fq.gz \
+    in2=chrysopogon_aciculatus_2.fq.gz \
+    out1=chrysopogon_aciculatus_1_clean1.fq \
+    out2=chrysopogon_aciculatus_2_clean2.fq
 
 # Themeda avenacea (AQ0476980) [AN20TSCR000216] --> next one to download
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-mv AN20T012_CKDL200169515-1a-AK5845-AK6676_HLTF5DSXY_L3_1.fq.gz themeda_avenacea_1.fq.gz
-mv AN20T012_CKDL200169515-1a-AK5845-AK6676_HLTF5DSXY_L3_2.fq.gz themeda_avenacea_2.fq.gz
+    in1=themeda_avenacea_1.fq.gz \
+    in2=themeda_avenacea_2.fq.gz \
+    out1=themeda_avenacea_1_clean1.fq \
+    out2=themeda_avenacea_2_clean2.fq
 
 # Andropogon gerardii (USA: Kansas, Ellis) [AN20NSCR000358]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-mv AN20N001_CKDL200149282-1a-AK4958-7UDI938_HNLFVDSXX_L1_1.fq.gz andropogon_gerardii_1.fq.gz
-mv AN20N001_CKDL200149282-1a-AK4958-7UDI938_HNLFVDSXX_L1_2.fq.gz andropogon_gerardii_2.fq.gz
+    in1=andropogon_gerardii_1.fq.gz \
+    in2=andropogon_gerardii_2.fq.gz \
+    out1=andropogon_gerardii_1_clean1.fq \
+    out2=andropogon_gerardii_2_clean2.fq
 
 # Ischaemum koenigii (Pasquet 1196) [AN21TNTL0138]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-mv AN21TNTL0138_CKDL210009738-1a-AK6510-AK6670_H5C7LDSX2_L4_1.fq.gz ischaemum_koenigii_1.fq.gz
-mv AN21TNTL0138_CKDL210009738-1a-AK6510-AK6670_H5C7LDSX2_L4_2.fq.gz ischaemum_koenigii_2.fq.gz
+    in1=ischaemum_koenigii_1.fq.gz \
+    in2=ischaemum_koenigii_2.fq.gz \
+    out1=ischaemum_koenigii_1_clean1.fq \
+    out2=ischaemum_koenigii_2_clean2.fq
 
 # Coix lacryma-jobi (PI 324509) [AN21TSTL0025]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
-
-mv AN21TS25_CKDL210002323-1a-AK6532-AK6712_HVGWVDSXY_L3_1.fq.gz coix_lacryma_jobi_1.fq.gz
-mv AN21TS25_CKDL210002323-1a-AK6532-AK6712_HVGWVDSXY_L3_2.fq.gz coix_lacryma_jobi_2.fq.gz
+    in1=coix_lacryma_jobi_1.fq.gz \
+    in2=coix_lacryma_jobi_2.fq.gz \
+    out1=coix_lacryma_jobi_1_clean1.fq \
+    out2=coix_lacryma_jobi_2_clean2.fq
 
 # Arthraxon junnarensis (Kew 88886) [AN21TSTL0030]
 /home/mbb262/bioinformatics/bbmap/bbduk.sh \
-    in1= \
-    in2=\
-    out1=_clean1.fq \
-    out2=_clean2.fq
+    in1=arthraxon_junnarensis_1.fq.gz \
+    in2=arthraxon_junnarensis_2.fq.gz \
+    out1=arthraxon_junnarensis_1_clean1.fq \
+    out2=arthraxon_junnarensis_2_clean2.fq
 
-mv AN21TS30_CKDL210002324-1a-AK6516-AK4469_HVGWVDSXY_L4_1.fq.gz arthraxon_junnarensis_1.fq.gz
-mv AN21TS30_CKDL210002324-1a-AK6516-AK4469_HVGWVDSXY_L4_2.fq.gz arthraxon_junnarensis_2.fq.gz
+# Skipping other team's genomes
 
+
+# ----------------------------------------
+# Run fastqc on two random genomes
+# to see if there are any enriched kmers
+# ----------------------------------------
+
+/programs/FastQC-0.11.8/fastqc ischaemum_koenigii_1.fq.gz ischaemum_koenigii_2.fq.gz
+
+/programs/FastQC-0.11.8/fastqc zea_diploperennis_1.fq.gz zea_diploperennis_2.fq.gz
+
+/programs/FastQC-0.11.8/fastqc schizachyrium_fragile_1.fq.gz schizachyrium_fragile_2.fq.gz
