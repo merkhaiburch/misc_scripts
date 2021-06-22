172d7c62a0445c25f00bdbc487f3c7a5d763edfa
Merritt Burch <merrittbburch@gmail.com>
2021-06-22 15:06:30 -0400

    grm for nam

--- tassel/calculate_grm.sh
+++ tassel/calculate_grm.sh
@@ -45,37 +45,33 @@ gzip /workdir/mbb262/filtered_NAM_phg_snps.vcf
     -endPlugin \
     -export /workdir/mbb262/kinship_filtered_NAM_phg_snps.txt
 
-# if the above doesn't work, try a kinship matirx on the whole genome file
 
 
 # -----------------------------------------
 #  NAM SNPs and GRM --> HARE regions only
 # -----------------------------------------
 
-# # bed file from HARE genes in v3, filtering SNPs within these intervals
-# # Filter data using tassel using MAF 
-# /home/mbb262/bioinformatics/tassel-5-standalone/run_pipeline.pl \
-#     -debug /workdir/mbb262/nam_hare_downsample_debug.log \
-#     -Xmx1000g \
-#     -maxThreads 86 \
-#     -importGuess /workdir/workdir/NAM_phg_snps.vcf -noDepth \
-#     -FilterSiteBuilderPlugin \
-#     -siteMinAlleleFreq 0.01 \
-#     -siteMaxAlleleFreq 0.99 \
-#     -removeSitesWithIndels true \
-#     -bedFile ./hare_v5_intervals.bed \
-#     -endPlugin \
-#     -export /workdir/mbb262/filtered_hare_nam_phg_snps.vcf \
-#     -exportType VCF
+# bed file from HARE genes in v3, filtering SNPs within these intervals
+# Filter data using tassel using MAF 
+/home/mbb262/bioinformatics/tassel-5-standalone/run_pipeline.pl \
+    -debug /workdir/mbb262/nam_hare_downsample_debug.log \
+    -Xmx1000g \
+    -maxThreads 86 \
+    -importGuess /workdir/mbb262/NAM_phg_snps.vcf -noDepth \
+    -FilterSiteBuilderPlugin \
+    -bedFile ./hare_v5_intervals.bed \
+    -endPlugin \
+    -export /workdir/mbb262/filtered_hare_nam_phg_snps.vcf \
+    -exportType VCF
 
-# gzip /workdir/mbb262/filtered_hare_nam_phg_snps.vcf
+bgzip --threads 80 /workdir/mbb262/filtered_hare_nam_phg_snps.vcf
 
 # calculate kinship genome wide using a subsetted vcf
 /home/mbb262/bioinformatics/tassel-5-standalone/run_pipeline.pl \
     -debug /workdir/mbb262/nam_hare_calc_grm_debug.log \
     -Xmx1000g \
     -maxThreads 86 \
-    -importGuess /workdir/mbb262/filtered_NAM_phg_snps.vcf.gz -noDepth \
+    -importGuess /workdir/mbb262/filtered_hare_nam_phg_snps.vcf.gz -noDepth \
     -KinshipPlugin \
     -method Centered_IBS \
     -endPlugin \
@@ -83,7 +79,7 @@ gzip /workdir/mbb262/filtered_NAM_phg_snps.vcf
 
 
 # --------------------
-#  282 SNPs and GRM
+#  282 SNPs filter and calc GRM
 # --------------------
 
 # Filter data using tassel using MAF 
@@ -112,7 +108,7 @@ gzip /workdir/mbb262/filtered_NAM_phg_snps.vcf
 
 
 # -----------------------------------------
-#  282 SNPs and GRM --> HARE regions only
+#  282 SNPs, filter, calc GRM --> HARE regions only
 # -----------------------------------------
 
 # Filter data using tassel using MAF 
@@ -146,31 +142,3 @@ gzip /workdir/mbb262/filtered_hare_goodman_phg_snps.vcf
 
 
 
-
-
-
-
-# Split results into different chromosomes
-bgzip -c /workdir/ag2484/NAM_phg_snps.vcf > /workdir/mbb262/NAM_phg_snps.vcf.gz
-tabix -p vcf /workdir/mbb262/NAM_phg_snps.vcf.gz
-
-# loop extract's each chromosome's info
-for CHROM in {1..10}
-do
-    echo "Start analyzing chromosome ${CHROM}"
-    tabix /workdir/mbb262/NAM_phg_snps.vcf.gz chr${CHROM} > nam_chr${CHROM}.vcf
-    echo "End analyzing chromosome ${CHROM}"
-done
-
-
-
-
-# Other non-functional code
-# randomly subset 10M sites genome wide using jvarkit/downsamplevcf
-java -jar /home/mbb262/bioinformatics/jvarkit/dist/downsamplevcf.jar \
-    /workdir/mbb262/filtered_goodman_phg_snps.vcf \
-    -o downsampled_10M_filtered_goodman_phg_snps.vcf.gz \
-    -n 10000000
-
-bcftools view --header-only /workdir/mbb262/filtered_goodman_phg_snps.vcf > subsample_goodman.vcf
-bcftools view --no-header input.vcf | awk '{printf("%f\t%s\n",rand(),$0);}' | sort -t $'\t' -k1,1g | cut -f2-  | head -n 1000 >>  subsample.vcf
