e4717c6fb4e40c012230158c06815412da1794ff
Merritt Khaipho-Burch <merrittbburch@gmail.com>
2021-07-23 14:18:11 -0400

    list all plugins in tassel

---
+++ tassel/list_plugins.bash
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------
+# Author.... Merritt Khaipho-Burch
+# Contact... mbb262@cornell.edu
+# Date...... 2021-07-23 
+#
+# Description 
+#   - How to tell if Tassel has the plugin you want
+#   - AKA: list all tassel plugins
+# ---------------------------------------------------------------
+
+# General
+./run_pipeline.pl -ListPlugins
+
+# On biohpc
+/home/mbb262/bioinformatics/tassel-5-standalone/run_pipeline.pl -ListPlugins
