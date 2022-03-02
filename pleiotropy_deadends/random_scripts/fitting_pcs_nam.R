c9ec0322b68cc7fa76492d3023165531ea684d36
Merritt Khaipho-Burch <merrittbburch@gmail.com>
2022-03-02 15:46:03 -0500

    fitting pcs part 2

--- pleiotropy_deadends/random_scripts/fitting_pcs_nam.R
+++ pleiotropy_deadends/random_scripts/fitting_pcs_nam.R
@@ -1,4 +1,22 @@
-phenos_nam <- c("01_nam_phenotypes_Buckler_2009.txt")
+phenos_nam <- c("01_nam_phenotypes_Buckler_2009.txt", 
+                "02_nam_phenotypes_Peiffer_2013.txt",
+                "03_nam_phenotypes_Hung_2012_1.txt", 
+                "04_nam_phenotypes_Wallace_2014.txt",
+                "05_nam_phenotypes_Peiffer_2014.txt", 
+                "06_nam_phenotypes_Hung_2012.txt",
+                "07_nam_phenotypes_Poland_2011.txt", 
+                "08_nam_phenotypes_Brown_2011.txt",
+                "09_nam_phenotypes_Bian_2014_Kump_2011.txt", 
+                "10_nam_phenotypes_Cook_2012.txt",
+                "11_nam_phenotypes_Olukolu_2014.txt", 
+                "12_nam_phenotypes_Benson_2015.txt",
+                "13_nam_phenotypes_Diepenbrock_2017.txt", 
+                "14_nam_phenotypes_Tian_2011.txt", 
+                "15_nam_phenotypes_Olukolu_2016.txt", 
+                "16_nam_phenotypes_Foerster_2015.txt")
+
+# Format names for export
+phenos_nam_export <- gsub("phenotypes_", "phenotypes_permuted10x_", phenos_nam)
 
 # Path to files
 phenopath_nam <- "~/Downloads/workdir/mbb262/phenotypes/nam/"
@@ -12,54 +30,106 @@ midway_pcs <- data.table::fread("~/Downloads/workdir/mbb262/03_pcs/nam/midwayGen
 j <- 1
 p <- 1
 
-# Load in phenotype file 
-phenotype <- data.table::fread(paste(phenopath_nam, phenos_nam[j], sep = ""))
-phenotype <- phenotype[,1:2]
 
-# Change taxa name (currently is <Trait>)
-colnames(phenotype)[1] <- "Taxa"
-
-# join phenotypes and PCs
-phenotype <- merge(phenotype, global_pcs, by = "Taxa")
-pheno_main_pcs <- merge(phenotype, main_pcs, by = "Taxa")
-pheno_midway_pcs <- merge(phenotype, midway_pcs, by = "Taxa")
-
-# Fit two models to the same trait??
-fit_main <- lm(DTA_BLUP.Buckler_2009_nam ~ ., data = pheno_main_pcs[,-1])
-fit_midway <- lm(DTA_BLUP.Buckler_2009_nam ~ ., data = pheno_midway_pcs[,-1])
-
-# Grab fitted values and residuals for both models??
-y_hat_main <- cbind(pheno_main_pcs[,1], fit_main$fitted.values)
-residual_y_main <- cbind(pheno_main_pcs[,1], fit_main$residuals)
-y_hat_midway <- cbind(pheno_midway_pcs[,1], fit_midway$fitted.values)
-residual_y_midway <- cbind(pheno_midway_pcs[,1], fit_midway$residuals)
-
-# Change column names to traits
-colnames(y_hat)[2] <- trait_ids[p]
-colnames(residual_y)[2] <- trait_ids[p]
-
-
-# Permute trait 10X times
-temp <- lapply(seq_len(10), function(perm) {
-  message("Permutation: ", perm)
+# Iterate through phenotypes
+pheno_files <- lapply(seq_len(length(phenos_nam)), function(j) {
+  message("I am on phenotype file ", phenos_nam[j])
   
-  # Collect phenotypes outside of loop
-  perm_adjusted_y_hat <- pcs_pheno[,1] %>% data.frame()
+  # Load in phenotype file 
+  phenotype <- data.table::fread(paste(phenopath_nam, phenos_nam[j], sep = ""))
+  phenotype <- phenotype[,1:3] # SUBSET JUST FOR TESTING
   
-  # subsample rows
-  perm_df <- cbind(residual_y[,1], residual_y[sample(nrow(residual_y)), -1])
+  # Swap out semicolons in name with underscors
+  colnames(phenotype) <- gsub(";", "_", colnames(phenotype))
   
-  # Add back y_hat
-  add_y_hat <- merge(perm_df, y_hat, by = "Taxa")
-  add_y_hat <- cbind(add_y_hat[,1], rowSums(add_y_hat[,-1]))
+  # Change taxa name (currently is <Trait>)
+  colnames(phenotype)[1] <- "Taxa"
   
-  # Change column names
-  colnames(add_y_hat)[2] <- paste0(trait_ids[p], "_permutation_", perm)
+  # join phenotypes and PCs
+  phenotype_gpcs <- merge(phenotype, global_pcs, by = "Taxa")
+  pheno_main_pcs <- merge(phenotype_gpcs, main_pcs, by = "Taxa")
+  pheno_midway_pcs <- merge(phenotype_gpcs, midway_pcs, by = "Taxa")
   
-  # merge into outside list
-  perm_adjusted_y_hat <- merge(perm_adjusted_y_hat, add_y_hat, by = "taxa")
-  return(perm_adjusted_y_hat)
+  # Gather trait IDs
+  trait_ids <- colnames(pheno_main_pcs[, 2:ncol(phenotype)])
+  
+  # Iterate through each phenotype file, then each trait, permute 10x
+  all_traits_perms <- lapply(seq_len(length(trait_ids)), function(p) {
+    message("Permuting and adjusting trait: ", trait_ids[p])
+    fit_main <- lm(paste(trait_ids[p], "~ ."), data = pheno_main_pcs[,-1]) #-1 removes Taxa ids
+    fit_midway <- lm(paste(trait_ids[p], "~ ."), data = pheno_midway_pcs[,-1]) #-1 removes Taxa ids
+    
+    # Grab fitted values and residual
+    y_hat_main <- cbind(pheno_main_pcs[,1], fit_main$fitted.values)
+    residual_y_main <- cbind(pheno_main_pcs[,1], fit_midway$residuals)
+    y_hat_midway <- cbind(pheno_midway_pcs[,1], fit_midway$fitted.values)
+    residual_y_midway <- cbind(pheno_midway_pcs[,1], fit_midway$residuals)
+    
+    # Change column names to traits
+    colnames(y_hat_main)[2] <- trait_ids[p]
+    colnames(residual_y_main)[2] <- trait_ids[p]
+    colnames(y_hat_midway)[2] <- trait_ids[p]
+    colnames(residual_y_midway)[2] <- trait_ids[p]
+    
+    # Permute trait 10X times
+    temp <- lapply(seq_len(10), function(perm) {
+      message("Permutation: ", perm)
+      
+      # Create an order to permute traits with
+      sample_order <- data.frame("Taxa" = sample(residual_y_main$Taxa))
+      
+      # Collect phenotypes
+      perm_adjusted_y_hat_main <- pheno_main_pcs[,1] %>% data.frame()
+      perm_adjusted_y_hat_midway <- pheno_midway_pcs[,1] %>% data.frame()
+      
+      # subsample rows in the same order between main and midway
+      residual_y_main <- residual_y_main[order(match(residual_y_main$Taxa, sample_order$Taxa)), ]
+      residual_y_midway <- residual_y_midway[order(match(residual_y_midway$Taxa, sample_order$Taxa)), ]
+      
+      # Add back y_hat
+      add_y_hat_main <- merge(perm_df, y_hat, by = "Taxa")
+      add_y_hat_main <- cbind(add_y_hat_main[,1], rowSums(add_y_hat_main[,-1]))
+      
+      add_y_hat <- merge(perm_df, y_hat, by = "Taxa")
+      add_y_hat <- cbind(add_y_hat[,1], rowSums(add_y_hat[,-1]))
+      
+      # Change column names
+      colnames(add_y_hat)[2] <- paste0(trait_ids[p], "_permutation_", perm)
+      
+      # merge into outside list
+      perm_adjusted_y_hat <- merge(perm_adjusted_y_hat, add_y_hat, by = "taxa")
+      # return(perm_adjusted_y_hat)
+    })
+    # Combine all permutations for a single trait
+    lala <- Reduce(function(x, y) merge(x, y, by = "taxa") , temp)
+  })
+  # Combine all traits and their corresponding permutations
+  temp2 <- Reduce(function(x, y) merge(x, y, by = "taxa") , all_traits_perms)
+  
+  # Export file
+  data.table::fwrite(temp2,
+                     paste0("~/Downloads/workdir/mbb262/phenotypes/02_permuted_fast_association_data/goodman/", phenos_good_export[j]))
+  
 })
+  
 
-# Combine all permutations for a single trait
-lala <- Reduce(function(x, y) merge(x, y, by = "taxa") , temp)
+
+# ----------------------------------------------------------
+# Proof of concept reordering used in above function works
+# ----------------------------------------------------------
+
+test1 <- data.frame("Taxa" = c("A", "B", "C", "D"), "Value" = c(1,2,3,4))
+test2 <- data.frame("Taxa" = c("A", "B", "C", "D"), "Value" = c(11,12,13,14))
+sample_order <- data.frame("Taxa" = sample(test1$Taxa))
+
+# Shuffled order
+sample_order
+
+# Reorder both datasets according to randomized sample order
+test1
+test1[order(match(test1$Taxa, sample_order$Taxa)), ]
+test2
+test2[order(match(test2$Taxa, sample_order$Taxa)), ]
+
+
+
