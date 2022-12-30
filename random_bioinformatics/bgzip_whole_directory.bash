cbe92dbf46ce694715651d8676533a1131d43044
Merritt Khaipho-Burch <merrittbburch@gmail.com>
2022-12-30 10:12:36 -0500

    adding a parallel version of gzip

--- random_bioinformatics/bgzip_whole_directory.bash
+++ random_bioinformatics/bgzip_whole_directory.bash
@@ -4,14 +4,24 @@
 # Author.... Merritt Khaipho-Burch
 # Contact... mbb262@cornell.edu
 # Date...... 2021-07-16 
+# Updated... 2022-12-28
 #
 # Description 
 #   - Script to unzip all the files in a directory using bgzip
 #   - bgzip does not support globbing (i.e. bgzip *.gz)
 # ---------------------------------------------------------------
 
+# Parallel command to zip
+find ./*.txt -type f > zippable_files.list
+/programs/parallel/bin/parallel -j 80 "gzip {}" :::: ./zippable_files.list
+
+# Parallel command to unzip
+find ./*.gz -type f > unzippable_files.list
+/programs/parallel/bin/parallel -j 11 "gunzip {}" :::: ./unzippable_files.list
+
+
 # unzip
-for FILE in *.gz
+for FILE in *.txt
 do
     bgzip -d --threads 20 ${FILE}
 done
@@ -20,7 +30,7 @@ done
 # zip txt file
 for FILE in *.txt
 do
-    bgzip --threads 20 ${FILE}
+    bgzip --threads 40 ${FILE}
 done
 
 
