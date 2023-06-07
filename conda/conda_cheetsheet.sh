6f564ff9372849585fbdd6701276ba4fd25f5469
Merritt Khaipho-Burch <mbb262@cornell.edu>
2023-06-07 10:07:13 -0400

    updated conda cheatsheet

--- conda/conda_cheetsheet.sh
+++ conda/conda_cheetsheet.sh
@@ -8,6 +8,11 @@
 # Conda cheetsheet
 # ------------------------------------------------------------------------------
 
+
+# ------------------------------------
+# Environment creation and destruction
+# ------------------------------------
+
 # Create an environment 
 conda create --name myenvironment
 
@@ -23,6 +28,66 @@ conda activate myenvironment
 # Exit from an environment
 conda deactivate
 
+# Delete a specific environment
+conda remove -n myenvironment --all
+
+
+# ---------------------
+# Package Stuff
+# ---------------------
+
 # Look at all installed packages in an environment (outside then inside an environment)
 conda list -n myenvironment
 conda list
+
+# Install a specific package
+conda install numpy
+
+# Update a specific package
+conda update numpy
+
+# Update all packages
+conda update --all
+
+
+# ------------------------
+# Wipe conda installation
+# ------------------------
+
+# Close any versions of conda open
+
+# Go to home directory
+cd
+
+# Show available versions of conda
+conda info
+
+# Start removing stuff (do in a screen session)
+rm -rf /home/mbb262/.condarc
+rm -v -rf /home/mbb262/miniconda
+
+# For future reference (on cbsu)
+rm -v -rf /home2/mbb262/.conda
+
+# Download miniforge
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+
+# close out of terminal, reopen.
+
+# Run, answer questions and say yes. One question will ask where I want this installed, /home2/mbb262
+sh miniforge3.sh
+
+# close out of terminal, reopen 
+
+# Check conda, update if necessary
+conda info
+
+# Install mamba, a faster solver for conda
+# https://www.anaconda.com/blog/a-faster-conda-for-a-growing-community
+conda install -n base conda-libmamba-solver
+conda config --set solver libmamba
+
+
+
+
+
